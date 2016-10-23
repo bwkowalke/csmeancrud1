6a5e659578afeead91eaea8a4094e628e2bd0d0e
Brad Kowalke <bwkowalke@gmail.com>
2016-10-23 15:42:32 -0500

    app scaffolding

---
+++ client/app/app.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('csmeancrudApp', [
+  'csmeancrudApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'ui.bootstrap'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
---
+++ client/components/navbar/navbar.controller.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+class NavbarController {
+  //start-non-standard
+  menu = [{
+    'title': 'Home',
+    'state': 'main'
+  }];
+
+  isCollapsed = true;
+  //end-non-standard
+
+
+}
+
+angular.module('csmeancrudApp')
+  .controller('NavbarController', NavbarController);
